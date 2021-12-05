405dfe1fb3934122571011855754c7f565eecd72
softplus <softplus@gmail.com>
2021-12-05 18:13:35 +0100

    removed temp

--- 6keyz--delete/kai6b/rules.mk
+++
@@ -1,8 +0,0 @@
-# unique to kai6 device
-
-#LED_MATRIX_ENABLE = yes		# Enable LED matrix for key lights
-#LED_MATRIX_DRIVER = custom	# Use our custom pin-mapped LED driver
-RGB_MATRIX_ENABLE = yes		# Enable RGB matrix
-RGB_MATRIX_DRIVER = custom	# Select WS2812 driver for RGB matrix
-# our special driver
-SRC += ../common/ledpins_as_matrix.c	# ... and add our custom code for the LED driver
